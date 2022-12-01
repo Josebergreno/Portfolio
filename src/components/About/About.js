d72ee32f5f0cc7fa80d379da99c526eadef79836
Joseph Bergren <bergren.bergren@yahoo.com>
2022-12-01 08:25:23 -0600

    made header responsive

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,24 @@
 import styles from "./About.module.css";
 import logo from "../../UI/logo-cropped.svg";
+import { Typography } from "@mui/material";
 
 const About = (props) => {
   console.log(logo);
   return (
     <div className={styles["container"]}>
-      <h1 className={styles["header"]}>Hi, I'm Joe. Web Developer.</h1>
+      <div className={styles["header-container"]}>
+        <Typography
+          variant="h2"
+          sx={{
+            fontSize: "10vmin",
+            color: "white",
+            gridColumn: "2/6",
+          }}
+        >
+          Hi, I'm Joe. Web Developer
+        </Typography>
+      </div>
+
       <div className={styles["logo-container"]}>
         <img className={styles["logo"]} src={logo} alt={"Logo"} />
       </div>
