6e6b394a007084319829a502ae23be612df3ef4c
Joseph Bergren <bergren.bergren@yahoo.com>
2022-12-08 16:10:17 -0700

    added form validation

--- src/components/Pages/PageComponents/Contact Page/ContactPage.js
+++ src/components/Pages/PageComponents/Contact Page/ContactPage.js
@@ -1,18 +1,76 @@
 import React from "react";
 import styles from "./ContactPage.module.css";
+import { useState } from "react";
 
 const ContactPage = () => {
-  const emailClickHandler = () => {
-    console.log("link");
-    // const link =
-    // "mailto:bergren.bergren@yahoo.com" +
-    // "&subject=" +
-    // encodeURIComponent(
-    //   document.getElementsByClassName("This is my subject")
-    // ) +
-    // "&body=" +
-    // encodeURIComponent(document.getElementById("myText").value);
+  // email validation
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [emailValid, setEmailValid] = useState(false);
+  const [emailWasTouched, setEmailWasTouched] = useState(false);
+  // subject validation
+  const [enteredSubject, setEnteredSubject] = useState("");
+  const [subjectValid, setSubjectValid] = useState(false);
+  const [subjectWasTouched, setSubjectWasTouched] = useState(false);
+  // // body validation
+  const [enteredBody, setEnteredBody] = useState("");
+  const [bodyValid, setBodyValid] = useState(false);
+  const [bodyWasTouched, setBodyWasTouched] = useState(false);
+
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const emailInputIsInvalid = !emailValid && emailWasTouched;
+
+  const emailInputClasses = emailInputIsInvalid
+    ? `${styles["input-fields--invalid"]}`
+    : `${styles["input-fields--valid"]}`;
+
+  const emailHandler = (e) => {
+    setEnteredEmail(e.target.value);
+    if (e.target.value.trim() !== "" && e.target.value.includes("@")) {
+      setEmailValid(true);
+      setEmailWasTouched(true);
+    } else setEmailValid(false);
+    setEmailWasTouched(true);
   };
+
+  const subjectInputIsInvalid = !subjectValid && subjectWasTouched;
+
+  const subjectInputClasses = subjectInputIsInvalid
+    ? `${styles["input-fields--invalid"]}`
+    : `${styles["input-fields--valid"]}`;
+
+  const subjectHandler = (e) => {
+    setEnteredSubject(e.target.value);
+    if (e.target.value.trim() !== "") {
+      setSubjectValid(true);
+      setSubjectWasTouched(true);
+    } else setSubjectValid(false);
+    setSubjectWasTouched(true);
+  };
+
+  const bodyInputIsInvalid = !bodyValid && bodyWasTouched;
+
+  const bodyInputClasses = bodyInputIsInvalid
+    ? `${styles["body-input--invalid"]}`
+    : `${styles["body-input--valid"]}`;
+
+  const bodyHandler = (e) => {
+    setEnteredBody(e.target.value);
+    if (e.target.value.trim() !== "") {
+      setBodyValid(true);
+      setBodyWasTouched(true);
+    } else setBodyValid(false);
+    setBodyWasTouched(true);
+  };
+  const formHandler = (e) => {
+    if (!subjectValid || !emailValid || !bodyValid) {
+      e.preventDefault();
+    } else e.preventDefault();
+    setEnteredBody("");
+    setFormSubmitted(true);
+    console.log(formSubmitted);
+  };
+
   return (
     <>
       <div className={styles["contact-layout"]}>
@@ -26,27 +84,70 @@ const ContactPage = () => {
           </a>{" "}
           or by filling out the form below.
         </div>
-        <div className={styles["form-container"]}>
-          <input className={styles["input-fields"]} placeholder="Your Email" />
-          <input className={styles["input-fields"]} placeholder="Subject" />
+        {emailInputIsInvalid && (
+          <p className={styles["warning"]}>Email must be valid</p>
+        )}
+        {subjectInputIsInvalid && (
+          <p className={styles["warning"]}>Please enter a subject</p>
+        )}
+        {bodyInputIsInvalid && (
+          <p className={styles["warning"]}>Please don't leave body empty</p>
+        )}
+        <form className={styles["form-container"]}>
+          <label className={styles["hidden-label"]} htmlFor="Email">
+            Email
+          </label>
+          <input
+            onBlur={emailHandler}
+            onKeyDown={emailInputIsInvalid ? emailHandler : undefined}
+            type="email"
+            id="Email"
+            className={emailInputClasses}
+            placeholder={"Your e-mail"}
+          />
+          <label className={styles["hidden-label"]} htmlFor="Subject"></label>
+
+          <input
+            onBlur={subjectHandler}
+            onKeyDown={subjectInputIsInvalid ? subjectHandler : undefined}
+            type="text"
+            id="Subject"
+            className={subjectInputClasses}
+            placeholder="Subject"
+          />
+          <label className={styles["hidden-label"]} htmlFor="Body">
+            Body
+          </label>
           <textarea
-            className={styles["body-input"]}
+            onBlur={bodyHandler}
+            onKeyDown={bodyInputIsInvalid ? bodyHandler : undefined}
+            id="Body"
+            className={bodyInputClasses}
             placeholder="Please write any questions here, and I'll get back to you as soon as I can!"
             rows={6}
-            // onChange={handleChange}
           ></textarea>
           <button
+            type="submit"
             className={styles["form-button"]}
             size="small"
             variant="outlined"
-            onClick={() => emailClickHandler()}
+            onClick={formHandler}
           >
             Send
           </button>
-        </div>
+        </form>
       </div>
     </>
   );
 };
 
 export default ContactPage;
+
+// const link =
+// "mailto:bergren.bergren@yahoo.com" +
+// "&subject=" +
+// encodeURIComponent(
+//   document.getElementsByClassName("This is my subject")
+// ) +
+// "&body=" +
+// encodeURIComponent(document.getElementById("myText").value);
