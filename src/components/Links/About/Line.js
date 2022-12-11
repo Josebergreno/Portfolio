d6c629461b51190c39131aa0e40410657f1ce457
Joseph Bergren <bergren.bergren@yahoo.com>
2022-12-11 09:01:31 -0700

    rebuilt line in about link page

--- src/components/Links/About/Line.js
+++ src/components/Links/About/Line.js
@@ -1,28 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./Line.module.css";
-import { useRef, useEffect, useState } from "react";
+
 const Line = () => {
-  const ref = useRef();
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
+  const ref = useRef("");
 
-  useEffect(() => {
-    setWidth(ref.current.parentElement.offsetWidth);
-    setHeight(ref.current.parentElement.offsetHeight);
-  }, [width, height]);
+  console.log(ref === null || ref === undefined);
+  console.log(ref.current.parentElement);
 
-  const roundedWidth = Math.round(width / 6.7);
-  const arrayWidth = Array.from({ length: roundedWidth }, (val) => (val = 1));
-  console.log(height);
-  return (
-    <div ref={ref} className={styles["line"]}>
-      {arrayWidth.map((val) => (
-        <div key={Math.random()} className={styles["line-item"]}>
-          _
-        </div>
-      ))}
-    </div>
-  );
+  return <hr ref={ref} className={styles["line"]} />;
 };
 
 export default Line;
