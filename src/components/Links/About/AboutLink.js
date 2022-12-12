887b552e76c05f19a7692f8cb830b17db52fc205
Joseph Bergren <bergren.bergren@yahoo.com>
2022-12-12 12:15:23 -0700

    added glitch effect to about link header

--- src/components/Links/About/AboutLink.js
+++ src/components/Links/About/AboutLink.js
@@ -3,17 +3,47 @@ import logo from "../../UI/Icons/logo-cropped.svg";
 import avatar from "../../UI/Icons/avatar.svg";
 import { Link } from "react-router-dom";
 import Line from "./Line";
+import { useEffect, useState } from "react";
 
 const AboutLink = (props) => {
+  const [activeLetter, setActiveLetter] = useState("");
+  const [randomNum, setRandomNum] = useState(0);
+
+  const headerEffect = ["Hi I'm", "Joe.", "Web", "Developer"];
+  const splitArr = headerEffect.join("").split("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRandomNum(Math.floor(Math.random() * 21) + 1);
+      setActiveLetter(splitArr[randomNum]);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [activeLetter, randomNum, splitArr]);
+
   return (
     <>
       <Link to="/about" onClick={props.onClick} className={styles["container"]}>
         <div className={styles["heading-container"]}>
           <h1 className={styles["h1"]}>
-            <span className={styles["hi"]}>Hi, I'm</span>
-            <span className={styles["joe"]}> Joe.</span>
-            <span className={styles["web"]}>Web</span>
-            <div className={styles["dev"]}>Developer.</div>
+            {headerEffect.map((val) => (
+              <span
+                key={val}
+                className={styles[val === "Joe." ? "joe" : undefined]}
+              >
+                {val.split("").map((val, i) => (
+                  <span
+                    className={
+                      val === activeLetter && activeLetter !== " "
+                        ? styles["flicker"]
+                        : styles["span"]
+                    }
+                    key={Math.random()}
+                  >
+                    {val}
+                  </span>
+                ))}
+              </span>
+            ))}
           </h1>
         </div>
 
